37445960a338b8a4288a9f0f78e2cee1b8b64619
JINMEI Tatuya <jinmei@isc.org>
2012-12-23 14:52:57 -0800

    needed to include config.h

--- src/lib/asio_message_manager.cc
+++ src/lib/asio_message_manager.cc
@@ -12,6 +12,8 @@
 // OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 // PERFORMANCE OF THIS SOFTWARE.
 
+#include <config.h>
+
 #include <message_manager.h>
 #include <asio_message_manager.h>
 
